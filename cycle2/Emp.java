c89491ce80fad1cf2da7c13437afba1def10757f
thomasvg67 <thomasvgvpkl123@gmail.com>
2024-04-02 16:26:14 +0530

    cycle3

--- cycle2/Emp.java
+++ cycle2/Emp.java
@@ -13,24 +13,24 @@ esalary = sal;
 void getdata()
 {
 Scanner sc=new Scanner(System.in);
-System.out.println("enter employee id :");
+System.out.println("Enter employee id :");
 eno =sc.nextInt();
-System.out.println("enter employee name : ");
+System.out.println("Enter employee name : ");
 ename = sc.next();
-System.out.println("enter employee salary:");
+System.out.println("Enter employee salary:");
 esalary = sc.nextDouble();
 }
 void display()
 {
-System.out.println("employee id is : "+eno);
-System.out.println("employee name is : "+ename);
-System.out.println("employee salary is : "+esalary);
+System.out.println("Employee id is : "+eno);
+System.out.println("Employee name is : "+ename);
+System.out.println("Employee salary is : "+esalary);
 }
 public static void main(String[] args)
 {
 int a,i,fl;
 System.out.print("Name : Thomas V.G\nRollno : 58\nDate : 26/03/2024\nProgram : Employee Details\n");
-System.out.println("enter the number of employees:");
+System.out.println("Enter the number of employees:");
 Scanner sc1=new Scanner(System.in);
 int n=sc1.nextInt();
 Emp e1[]=new Emp[n];
@@ -43,10 +43,10 @@ e1[i].getdata();
 System.out.println("Employee details are:");
 for ( i = 0; i <n; i++)
 {
-System.out.println("details of employee "+i+ "are:");
+System.out.println("Details of employee "+(i+1)+ " are:");
 e1[i].display();
 }
-System.out.println("Enter employe id to be searched for:");
+System.out.println("Enter employee id to be searched for:");
 a=sc1.nextInt();
 for(i=0;i<n;i++)
 {
---
+++ cycle3/Shapes.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+public class Shapes {
+   
+    void area(int r1){
+        double Area_val = 3.14*r1*r1;
+        System.out.println("\nArea of Circle is Radius "+r1+" = "+Area_val);
+    }
+    void area(int a1,int b1){
+        int Area_val = a1*b1;
+        System.out.println("\nArea of Rectangle is with dimensions "+a1+" X "+b1+" = "+Area_val);
+    }
+    void area(int a1,int b1,int c1){
+        int Area_val = a1*b1*c1;
+        System.out.println("\nArea of Cuboid is with dimensions "+a1+" X "+b1+" X "+c1+" = "+Area_val);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nEnter the Length :");
+        int l = sc.nextInt();
+        System.out.println("Enter the Breadth :");
+        int b = sc.nextInt();
+        System.out.println("Enter the Height :");
+        int h = sc.nextInt();
+        System.out.println("Enter the Radius :");
+        int r = sc.nextInt();
+        Shapes obj1 = new Shapes();
+        obj1.area(r);
+        obj1.area(l,b);
+        obj1.area(l,b,h);
+    }
+
+}
